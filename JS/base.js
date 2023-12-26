4af1d96d0e4f67872c7c7114928d82075714a55b
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-25 23:52:52 -0300

    Botões adicionado em HTML e detecção de clique

---
+++ JS/base.js
@@ -0,0 +1,14 @@
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(function(button)
+{
+    button.addEventListener('click', function()
+    {
+        showButton(button.textContent);
+    });
+});
+
+function showButton(buttonText)
+{
+    document.querySelector('p').innerHTML += buttonText;
+}
